e5ac9972e465e8bc9fde1c3c790b05a85248cac9
Cédric Hotz <36566958+cedricHOTZ@users.noreply.github.com>
2020-03-06 22:27:12 +0100

    cedric
    Affichage des formations sur la page d'accueil.Form contact et envoi email.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UdemyCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
